bf2fc0c97d993b1839f013042027d57710350736
satret <artur-stepanov-03@mail.ru>
2024-10-05 02:26:45 +0300

    add TeapotController

--- blps_labs/src/main/java/blps/lab/security/controller/AuthController.java
+++ blps_labs/src/main/java/blps/lab/security/controller/AuthController.java
@@ -8,10 +8,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -23,7 +22,6 @@ public class AuthController {
     @Operation(summary = "Регистрация пользователя")
     @PostMapping("/sign-up")
     public JwtAuthenticationResponse signUp(@RequestBody @Valid SignUpRequest request) {
-        //todo добавить проверку на то что юзер существует
         return authenticationService.signUp(request);
     }
 
