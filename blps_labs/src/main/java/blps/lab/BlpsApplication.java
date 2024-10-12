61a021b196869a5e0fd12c2f0d142631b08e2313
Tr1-galki <aleksg2004@gmail.com>
2024-10-12 14:22:42 +0300

    add camunda to project

--- blps_labs/src/main/java/blps/lab/BlpsApplication.java
+++ blps_labs/src/main/java/blps/lab/BlpsApplication.java
@@ -1,9 +1,11 @@
 package blps.lab;
 
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class BlpsApplication {
 
 	public static void main(String[] args) {
