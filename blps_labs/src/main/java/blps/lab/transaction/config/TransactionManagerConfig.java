275569f985724338294780ed27d2ba664c913a99
Tr1-galki <aleksg2004@gmail.com>
2024-10-16 22:25:45 +0300

    sync changes

--- blps_labs/src/main/java/blps/lab/transaction/config/TransactionManagerConfig.java
+++ blps_labs/src/main/java/blps/lab/transaction/config/TransactionManagerConfig.java
@@ -7,23 +7,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.jta.JtaTransactionManager;
 
-@Configuration
-@EnableTransactionManagement
+//@Configuration
+//@EnableTransactionManagement
 public class TransactionManagerConfig {
-    @Bean(initMethod = "init", destroyMethod = "close")
-    public UserTransactionManager userTransactionManager() throws SystemException {
-        UserTransactionManager userTransactionManager = new UserTransactionManager();
-        userTransactionManager.setTransactionTimeout(300);
-        userTransactionManager.setForceShutdown(true);
-        return userTransactionManager;
-    }
-
-    @Bean
-    public JtaTransactionManager transactionManager() throws SystemException {
-        JtaTransactionManager jtaTransactionManager = new JtaTransactionManager();
-        jtaTransactionManager.setTransactionManager(userTransactionManager());
-        jtaTransactionManager.setUserTransaction(userTransactionManager());
-        return jtaTransactionManager;
-    }
+//    @Bean(initMethod = "init", destroyMethod = "close")
+//    public UserTransactionManager userTransactionManager() throws SystemException {
+//        UserTransactionManager userTransactionManager = new UserTransactionManager();
+//        userTransactionManager.setTransactionTimeout(300);
+//        userTransactionManager.setForceShutdown(true);
+//        return userTransactionManager;
+//    }
+//
+//    @Bean
+//    public JtaTransactionManager transactionManager() throws SystemException {
+//        JtaTransactionManager jtaTransactionManager = new JtaTransactionManager();
+//        jtaTransactionManager.setTransactionManager(userTransactionManager());
+//        jtaTransactionManager.setUserTransaction(userTransactionManager());
+//        return jtaTransactionManager;
+//    }
 }
 
--- blps_labs/src/main/java/blps/lab/transaction/service/TransactionService.java
+++ blps_labs/src/main/java/blps/lab/transaction/service/TransactionService.java
@@ -9,24 +9,28 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.jta.JtaTransactionManager;
 
 @Service
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class TransactionService {
-    private final JtaTransactionManager transactionManager;
+//    private final JtaTransactionManager transactionManager;
+
+//    public <T> T executeTransactional(String name, int timeout, Supplier<T> supplier) {
+//        try {
+//            Transaction transaction = transactionManager.createTransaction(name, timeout);
+//            try {
+//                T result = supplier.get();
+//                transaction.commit();
+//                System.err.println("Transaction completed successfully");
+//                return result;
+//            } catch (Exception e) {
+//                transaction.rollback();
+//                throw new TransactionTimeoutException();
+//            }
+//        } catch (Exception e) {
+//            throw new TransactionTimeoutException();
+//        }
+//    }
 
     public <T> T executeTransactional(String name, int timeout, Supplier<T> supplier) {
-        try {
-            Transaction transaction = transactionManager.createTransaction(name, timeout);
-            try {
-                T result = supplier.get();
-                transaction.commit();
-                System.err.println("Transaction completed successfully");
-                return result;
-            } catch (Exception e) {
-                transaction.rollback();
-                throw new TransactionTimeoutException();
-            }
-        } catch (Exception e) {
-            throw new TransactionTimeoutException();
-        }
+        return supplier.get();
     }
 }
