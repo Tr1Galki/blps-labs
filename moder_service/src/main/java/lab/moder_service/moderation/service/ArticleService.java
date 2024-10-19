4769f234dc36bd8719c148a9a10729873bf7fae0
Tr1-galki <aleksg2004@gmail.com>
2024-10-19 10:20:00 +0300

    add bitronix

--- moder_service/src/main/java/lab/moder_service/moderation/service/ArticleService.java
+++ moder_service/src/main/java/lab/moder_service/moderation/service/ArticleService.java
@@ -1,6 +1,7 @@
 package lab.moder_service.moderation.service;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 
 import lab.moder_service.moderation.entity.Article;
@@ -9,13 +10,19 @@ import lab.moder_service.transaction.service.TransactionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import static java.lang.Thread.sleep;
-
 @Service
 @RequiredArgsConstructor
 public class ArticleService {
     private final ArticleRepository articleRepository;
     private final TransactionService transactionService;
+
+    public List<Article> getAllArticles() {
+        return articleRepository.findArticlesByIsDraft(true);
+    }
+
+    public void deleteArticle(Article article) {
+        articleRepository.delete(article);
+    }
 
     public Article create(Article article) {
         return articleRepository.save(article);
---
+++ moder_service/src/main/java/lab/moder_service/schedule/ScheduleConfig.java
@@ -0,0 +1,30 @@
+package lab.moder_service.schedule;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+import lab.moder_service.moderation.service.ArticleService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Configuration
+@EnableScheduling
+@RequiredArgsConstructor
+@Slf4j
+public class ScheduleConfig {
+    private final ArticleService articleService;
+
+    @Scheduled(cron = "0 */1 * * * ?")
+    public void warnTimoutDrafts() {
+        log.info("Scheduler task starts");
+        articleService.getAllArticles().stream()
+                .filter(article -> article.getIsDraft()
+                        && article.getSentAt() != null
+                        && article.getSentAt().isBefore(Instant.now().minus(1, ChronoUnit.DAYS))
+                )
+                .forEach(article -> log.warn("Article with id {} did not reviewed more then day", article.getId()));
+    }
+}
